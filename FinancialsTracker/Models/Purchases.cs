3174ef52a23bbfc96c7b5ebd4b12ef15b929c9c0
millsmgb <millsmgb@gmail.com>
2015-01-29 14:24:00 +1100

    Added Purchases Controller

---
+++ FinancialsTracker/Models/Purchases.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FinancialsTracker.Models
+{
+    public class Purchase
+    {
+        public int PurchaseId { get; set; }
+        public int UserId { get; set;}
+        public string Name { get; set; }
+        public int Price { get; set; }
+        public int Quantity { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+        public System.DateTime DateTime { get; set; }
+        public string Location { get; set; }
+    }
+}
