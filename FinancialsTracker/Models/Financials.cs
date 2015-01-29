87c0d25611521952b8dc01786f814f640b3a20af
millsmgb <millsmgb@gmail.com>
2015-01-29 14:32:36 +1100

    test

---
+++ FinancialsTracker/Models/Financials.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FinancialsTracker.Models
+{
+    public class financialItem
+    {
+        public int fincancialItemId { get; set; }
+        public int UserId { get; set; }
+        public int Price { get; set; }
+        public int Quantity { get; set; }
+        public float Latitude { get; set; }
+        public float Longitude { get; set; }
+        public string DateTime { get; set; }
+
+    }
+}
