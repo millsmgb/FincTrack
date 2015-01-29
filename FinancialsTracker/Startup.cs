a4897dc757ffeb31b59d8c802476201db31c7852
millsmgb <millsmgb@gmail.com>
2015-01-29 13:38:18 +1100

    Initial Upload

---
+++ FinancialsTracker/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FinancialsTracker.Startup))]
+namespace FinancialsTracker
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
